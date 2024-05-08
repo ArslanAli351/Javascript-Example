9fc6e9659c4d117022168b95a7c8c93f568f828e
Arslan <muhammadarslanrajput12@gmail.com>
2024-05-08 22:04:15 +0500

    javascript example

---
+++ app.js
@@ -0,0 +1,70 @@
+
+// let fruits = ["apple", "banana", "oranges"];
+
+// // fruits.pop();
+
+// fruits.push(+"mango", "kharbooza");
+
+// console.log(fruits)
+
+
+
+// let fruits = ["apple", "banana", "oranges"];
+
+// fruits.shift(); 
+
+// fruits.unshift("mango", "kharbooza");
+
+// console.log(fruits);
+
+
+// let strArr = ["h", "a", "i", "d", "e", "r"];
+
+// console.log(strArr.reverse().join(""));
+
+
+// let userName = ["haider ali shah"];
+
+// let nameArr = userName.split("");
+// console.log(nameArr);
+
+
+
+
+
+// for (let i = 0; i < 10;i++ ) {
+//     console.log(i);
+// }
+
+// console.log('hello world');
+
+
+// for (let i = 100; i > 0; i--) {
+//     console.log("Current iteration:" + i);
+// }
+
+// for (let i = 0; i < 100; i = i + 2) {
+//     console.log("Current iteration:" + i);
+// }
+
+
+
+
+
+//forforforforforfor
+
+
+// let students = ["aslam", 'akram', 'muzammil', 'abdullah'];
+// let num=[2]
+// // console.log(num)
+// for (let i = 0; i <3; i++) {
+//     console.log('Hello ' +num [i])
+// }
+
+// let cleanCity = "karachi";
+// let userCity = prompt("In which city do you live?");
+
+// if (userCity.toLowerCase() === cleanCity.toLowerCase()) {
+// console.log(userCity)
+// }
+
